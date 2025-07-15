337f45b86528d7e119f86000dc9ed00b037679a8
Your Name <you@example.com>
2025-07-15 12:13:09 +0000

    Init

---
+++ my_project/models/source_object/dp_sap_invoices/output/dp_sap_invoices__invoices_c2.sql
@@ -0,0 +1,6 @@
+{{{{
+  config(
+    materialized = 'view',
+    schema = 'toto'
+    )
+}}}}
---
+++ my_project/models/source_object/dp_sap_invoices/transform/dp_sap_invoices__transform__invoices.sql
@@ -0,0 +1,8 @@
+{{{{
+  config(
+    materialized = 'table',
+    schema = 'toto'
+    )
+}}}}
+
+-- joins from headers and lines tables in input folder
